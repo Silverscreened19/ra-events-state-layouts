28b85117f0c976120dff95885420fe01e0af02c9
Olya Tatianina <olya.tatianina@gmail.com>
2024-01-16 16:31:35 +0400

    proptypes rule off

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Store from './components/Store'
+import "./App.css";
+import Store from "./components/Store";
 
 function App() {
-
   return (
     <>
-    {/* diewf */}
       <Store />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
